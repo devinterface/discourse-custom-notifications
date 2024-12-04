0c9dcaa313564431de91dcc3781a9f3256c89095
Loris Oppia <loris.oppia@devinterface.com>
2024-12-04 10:44:31 +0100

    Fix csv_exporter for all topics on search

--- assets/javascripts/discourse/helpers/csv_exporter.js
+++ assets/javascripts/discourse/helpers/csv_exporter.js
@@ -8,52 +8,92 @@ addBulkDropdownButton({
   allowSilent: true,
   actionType: "performAndRefresh",
   action: () => {
-    let topics_id = [];
-    const listItems = document.querySelectorAll('div[role="listitem"]');
+    let query = new URLSearchParams(window.location.search).get("q");
+    let topics_id_ajax = [];
+    let topic_id_selected = [];
+    let topic_id_final = [];
 
-    listItems.forEach((item) => {
-      const bulkSelectSpan = item.querySelector("span.bulk-select");
+    function fetchData(page) {
+      if (page > 10) {
+        return;
+      }
+      $.ajax({
+        url: `search?q=${encodeURIComponent(query)}&page=${page}`,
+        method: "GET",
+        dataType: "json",
+      }).done(async (data) => {
+        if (!data["posts"] || data["posts"].length === 0) {
+          await get_topics_selected();
+          return;
+        }
+        data["posts"].forEach((post) => {
+          if (post.topic_id) {
+            topics_id_ajax.push(post.topic_id);
+          }
+        });
+        fetchData(page + 1);
+      });
+    }
 
-      if (bulkSelectSpan) {
-        const checkbox = bulkSelectSpan.querySelector('input[type="checkbox"]');
+    fetchData(1);
 
-        if (checkbox && checkbox.checked) {
-          const topicDiv = item.querySelector("div[data-topic-id]");
-
-          if (topicDiv) {
-            const topicId = topicDiv.getAttribute("data-topic-id");
-            topics_id.push(topicId);
-          } else {
+    async function get_topics_selected() {
+      const listItems = document.querySelectorAll('div[role="listitem"]');
+      listItems.forEach((item) => {
+        const bulkSelectSpan = item.querySelector("span.bulk-select");
+        if (bulkSelectSpan) {
+          const checkbox = bulkSelectSpan.querySelector(
+            'input[type="checkbox"]'
+          );
+          if (checkbox && checkbox.checked) {
+            const topicDiv = item.querySelector("div[data-topic-id]");
+            if (topicDiv) {
+              topic_id_selected.push(
+                parseInt(topicDiv.getAttribute("data-topic-id"))
+              );
+            }
           }
         }
-      }
-    });
-    $.ajax({
-      type: "POST",
-      url: "/download_csv_topics",
-      data: { topics_id: topics_id },
-      xhrFields: {
-        responseType: "blob",
-      },
-      success: function (blob, status, xhr) {
-        const disposition = xhr.getResponseHeader("Content-Disposition");
-        const filename = disposition
-          ? disposition.match(/filename="(.+)"/)[1]
-          : "download.csv";
+      });
+      await get_filter();
+    }
 
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = filename;
-        document.body.appendChild(a);
-        a.click();
-        window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
-      },
-      error: function (xhr) {
-        console.error("Errore durante il download del CSV", xhr);
-      },
-    });
-    location.reload();
+    function get_filter() {
+      topics_id_ajax.filter((element) => {
+        if (topic_id_selected.includes(element)) {
+          topic_id_final.push(element);
+        }
+      });
+      export_csv();
+    }
+
+    function export_csv() {
+      $.ajax({
+        type: "POST",
+        url: "/download_csv_topics",
+        data: { topics_id: topic_id_final },
+        xhrFields: {
+          responseType: "blob",
+        },
+        success: function (blob, status, xhr) {
+          const disposition = xhr.getResponseHeader("Content-Disposition");
+          const filename = disposition
+            ? disposition.match(/filename="(.+)"/)[1]
+            : "download.csv";
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement("a");
+          a.href = url;
+          a.download = filename;
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+          document.body.removeChild(a);
+          location.reload();
+        },
+        error: function (xhr) {
+          console.error("Errore durante il download del CSV", xhr);
+        },
+      });
+    }
   },
 });
